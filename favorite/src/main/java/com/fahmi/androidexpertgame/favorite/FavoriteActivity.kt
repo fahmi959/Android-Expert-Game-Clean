3f851c2480fafb2c784983bb4c3523487c14c429
Fahmi <fahmiardiansyah959@students.unnes.ac.id>
2023-09-28 15:12:42 +0700

    Fahmi Fix Final

--- favorite/src/main/java/com/fahmi/androidexpertgame/favorite/FavoriteActivity.kt
+++ favorite/src/main/java/com/fahmi/androidexpertgame/favorite/FavoriteActivity.kt
@@ -3,7 +3,6 @@ package com.fahmi.androidexpertgame.favorite
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import androidx.recyclerview.widget.GridLayoutManager
 import com.fahmi.androidexpertgame.core.ui.GameAdapter
 import com.fahmi.androidexpertgame.detail.DetailGameActivity
